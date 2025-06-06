eb51a740ed1620c4a3c96739a398c2d49f506a40
https://github.com/AuraFrameFX-Dev <wehttam1989@gmail.com>
2025-06-06 02:07:12 -0600

    Add Compose compiler configuration with kotlinCompilerExtensionVersion

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,6 +67,10 @@ android {
         compose = true
     }
 
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.10"
+    }
+
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
