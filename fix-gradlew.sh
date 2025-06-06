c1aeb4ec79771b9c47d9a19eaf6859ad2bc3541b
https://github.com/AuraFrameFX-Dev <wehttam1989@gmail.com>
2025-06-06 02:10:53 -0600

    Add script to fix gradlew executable permissions

--- fix-gradlew.sh
+++ fix-gradlew.sh
@@ -2,21 +2,13 @@
 # Fix gradlew permissions and push to GitHub
 
 # Make gradlew executable
-chmod +x gradlew
-
-# Check if the file has executable permissions
-ls -la gradlew
-
-# Update git to track the file with executable permissions
-git update-index --chmod=+x gradlew
+chmod +x ./gradlew
 
 # Add the file to git
-git add gradlew
+git add ./gradlew
 
 # Commit the change
 git commit -m "Make gradlew executable"
 
-# Push to GitHub
+# Push to your branch (assuming you're on gradle-fixes)
 git push origin gradle-fixes
-
-echo "Done!"
