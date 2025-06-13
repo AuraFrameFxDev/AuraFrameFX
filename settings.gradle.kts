e311c2c2b43292063da39f96ca60959d7885b0de
AuraKai <wehttam1989@gmail.com>
2025-06-13 00:22:08 -0600

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,18 +1,25 @@
 // settings.gradle.kts
+@file:Suppress("UnstableApiUsage")
 
 pluginManagement {
     repositories {
+        google()
+        mavenCentral()
         gradlePluginPortal()
+        maven { url = uri("https://jitpack.io") }
+        maven { url = uri("https://maven.pkg.jetbrains.space/public/p/compose/dev") }
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
         google()
         mavenCentral()
         maven { url = uri("https://jitpack.io") }
         maven { url = uri("https://maven.pkg.jetbrains.space/public/p/compose/dev") }
     }
-    
-    plugins {
-        id("com.android.application") version "8.14.2" // <--- ADD THIS LINE
-        id("org.jetbrains.compose") version "1.6.11"
-        id("org.openapi.generator") version "7.5.0"
-        // ... other plugins
-    }
 }
+
+rootProject.name = "AuraFrameFx"
+include(":app")
